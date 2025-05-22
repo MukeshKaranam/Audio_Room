008badb3f0de9a88544094d67c3afacacdf50161
MukeshKaranam <138287881+MukeshKaranam@users.noreply.github.com>
2025-05-22 23:49:13 +0530

    Add files via upload

---
+++ server/src/stream-client.ts
@@ -0,0 +1,4 @@
+import { StreamClient } from "@stream-io/node-sdk";
+const apiKey="gqg4v3dfs7m8"
+const secret="gqan2kwrz7xehp6n4wanm8wgnm6pur49seacyvxw7nqtemp6mxm6nuk5wsa5dk9q"
+export const client = new StreamClient(apiKey, secret);
